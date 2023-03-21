fd4a11795f344882ee87d6743b92ad70a43d93f5
Zixuan Xu <zxuanxu@hotmail.com>
2023-03-21 15:56:23 -0700

    Add CodeMR analysis

---
+++ project/codemr/SPRINT2Submission/html/html_report_project/htmlx/modeldata/sunburst_data.js
@@ -0,0 +1,146 @@
+function EQ_GET_DATA(){ 
+	  var ret = {
+"name": " project working set: {project}", "value":412, 
+"prmetrics":{"5":1,"6":1,"7":1,"8":1,"9":1,"10":1,"11":1},
+"prmetricvalues":{"5":0,"6":9,"7":0,"8":412,"9":8,"10":5,"11":19},
+"children": [ {
+"name": "com.project.model", "key": "l", "value":196, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1},
+"pmetricvalues":{"17":3,"4":1,"12":0,"13":0,"14":2,"3":1,"1":1,"0":1,"18":1.0,"6":2,"19":0.0,"8":196,"20":0.0,"21":0,"2":1,"15":5,"22":3,"16":48},
+"children":[
+{
+"name": "DailyActivityDetail","key": "m","value":106, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":2,"31":1,"32":2,"33":3,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":2,"40":1,"4":2,"8":2,"2":1,"3":1,"1":2},
+"metricvalues":{"23":0,"24":0,"17":3,"25":10,"26":0.0,"27":93,"28":0,"29":1,"0":2,"30":26,"31":26,"32":0.692,"33":0.792,"34":0,"35":0.436,"21":0,"36":0,"37":0,"22":3,"38":0,"39":0,"16":26,"40":0,"4":2,"8":106,"2":1,"3":1,"1":2}
+},
+{
+"name": "DailyActivitySummary","key": "r","value":90, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":2,"30":2,"31":1,"32":2,"33":3,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":2,"40":1,"4":2,"8":2,"2":1,"3":1,"1":2},
+"metricvalues":{"23":0,"24":0,"17":3,"25":8,"26":0.0,"27":79,"28":0,"29":1,"0":2,"30":22,"31":22,"32":0.628,"33":0.738,"34":0,"35":0.424,"21":0,"36":0,"37":0,"22":3,"38":0,"39":0,"16":22,"40":0,"4":2,"8":90,"2":1,"3":1,"1":2}
+}
+]
+},{
+"name": "com.project.repository", "key": "u", "value":26, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1},
+"pmetricvalues":{"17":3,"4":1,"12":2,"13":2,"14":0,"3":1,"1":1,"0":1,"18":0.4,"6":2,"19":1.0,"8":26,"20":0.4,"21":1,"2":1,"15":3,"22":2,"16":13},
+"children":[
+{
+"name": "DetailRepository","key": "v","value":14, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"40":1,"4":1,"8":1,"2":1,"3":1,"16":1,"1":1},
+"metricvalues":{"23":2,"24":0,"17":3,"25":0,"26":0.0,"27":7,"28":0,"29":1,"0":1,"30":7,"31":7,"32":0.0,"33":0.0,"34":0,"35":0.381,"21":1,"36":1,"37":0,"22":2,"38":0,"39":1,"40":0,"4":1,"8":14,"2":1,"3":1,"16":7,"1":1}
+},
+{
+"name": "SummaryRepository","key": "w","value":12, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"40":1,"4":1,"8":1,"2":1,"3":1,"16":1,"1":1},
+"metricvalues":{"23":0,"24":0,"17":3,"25":0,"26":0.0,"27":6,"28":0,"29":1,"0":1,"30":6,"31":6,"32":0.0,"33":0.0,"34":0,"35":0.083,"21":1,"36":0,"37":0,"22":2,"38":0,"39":0,"40":0,"4":1,"8":12,"2":1,"3":1,"16":6,"1":1}
+}
+]
+},{
+"name": "com.project", "key": "j", "value":72, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1},
+"pmetricvalues":{"17":1,"4":1,"12":1,"13":0,"14":1,"3":1,"1":1,"0":1,"18":0.0,"6":1,"19":0.0,"8":72,"20":1.0,"21":1,"2":1,"15":0,"22":0,"16":8},
+"children":[
+{
+"name": "Application","key": "k","value":72, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":2,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":2,"8":2,"2":1,"3":1,"1":1},
+"metricvalues":{"23":4,"24":0,"17":2,"25":0,"26":0.0,"27":69,"28":0,"29":1,"0":1,"30":3,"31":14,"32":0.0,"33":0.0,"34":0,"35":0.562,"21":2,"36":2,"37":10,"22":0,"38":1,"39":2,"16":8,"40":0,"4":2,"8":72,"2":1,"3":1,"1":1}
+}
+]
+},{
+"name": "com.project.controller", "key": "n", "value":84, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1},
+"pmetricvalues":{"17":2,"4":1,"12":2,"13":0,"14":2,"3":1,"1":1,"0":1,"18":0.0,"6":2,"19":0.0,"8":84,"20":1.0,"21":2,"2":1,"15":0,"22":0,"16":22},
+"children":[
+{
+"name": "DailyActivitySummaryController","key": "s","value":43, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":1,"8":1,"2":1,"3":1,"1":1},
+"metricvalues":{"23":5,"24":0,"17":2,"25":1,"26":0.0,"27":38,"28":0,"29":1,"0":1,"30":5,"31":21,"32":0.0,"33":0.0,"34":0,"35":0.267,"21":2,"36":2,"37":12,"22":0,"38":0,"39":3,"16":11,"40":0,"4":1,"8":43,"2":1,"3":1,"1":1}
+},
+{
+"name": "DailyActivityDetailController","key": "o","value":41, 
+"metrics":{"23":2,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":2,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":1,"8":1,"2":2,"3":1,"1":1},
+"metricvalues":{"23":6,"24":0,"17":2,"25":1,"26":0.0,"27":36,"28":0,"29":1,"0":2,"30":5,"31":22,"32":0.0,"33":0.0,"34":0,"35":0.45,"21":2,"36":2,"37":12,"22":0,"38":0,"39":4,"16":11,"40":0,"4":1,"8":41,"2":2,"3":1,"1":1}
+}
+]
+},{
+"name": "com.project.service", "key": "p", "value":34, 
+"pmetrics":{"4":1,"12":1,"13":1,"14":1,"3":1,"1":1,"0":1,"6":1,"8":1,"2":1,"15":1,"16":1},
+"pmetricvalues":{"17":3,"4":1,"12":2,"13":0,"14":2,"3":1,"1":1,"0":1,"18":0.5,"6":2,"19":0.0,"8":34,"20":0.5,"21":2,"2":1,"15":2,"22":1,"16":10},
+"children":[
+{
+"name": "DailyActivitySummaryService","key": "t","value":19, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":1,"8":1,"2":1,"3":1,"1":1},
+"metricvalues":{"23":2,"24":0,"17":3,"25":1,"26":0.0,"27":15,"28":0,"29":1,"0":1,"30":5,"31":9,"32":0.0,"33":0.0,"34":0,"35":0.267,"21":2,"36":2,"37":4,"22":1,"38":0,"39":0,"16":5,"40":0,"4":1,"8":19,"2":1,"3":1,"1":1}
+},
+{
+"name": "DailyActivityDetailService","key": "q","value":15, 
+"metrics":{"23":1,"24":1,"25":1,"26":1,"27":1,"28":1,"29":1,"0":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1,"16":1,"40":1,"4":1,"8":1,"2":1,"3":1,"1":1},
+"metricvalues":{"23":3,"24":0,"17":3,"25":1,"26":0.0,"27":11,"28":0,"29":1,"0":1,"30":5,"31":10,"32":0.0,"33":0.0,"34":0,"35":0.45,"21":2,"36":2,"37":5,"22":1,"38":0,"39":1,"16":5,"40":0,"4":1,"8":15,"2":1,"3":1,"1":1}
+}
+]
+}]
+ }
+;
+return ret;
+}
+var EQ_METRIC_MAP = {};
+EQ_METRIC_MAP["C3"] =0;
+EQ_METRIC_MAP["Complexity"] =1;
+EQ_METRIC_MAP["Coupling"] =2;
+EQ_METRIC_MAP["Lack of Cohesion"] =3;
+EQ_METRIC_MAP["Size"] =4;
+EQ_METRIC_MAP["Number of Highly Problematic Classes"] =5;
+EQ_METRIC_MAP["Number of Entities"] =6;
+EQ_METRIC_MAP["Number of Problematic Classes"] =7;
+EQ_METRIC_MAP["Class Lines of Code"] =8;
+EQ_METRIC_MAP["Number of External Packages"] =9;
+EQ_METRIC_MAP["Number of Packages"] =10;
+EQ_METRIC_MAP["Number of External Entities"] =11;
+EQ_METRIC_MAP["Efferent Coupling"] =12;
+EQ_METRIC_MAP["Number of Interfaces"] =13;
+EQ_METRIC_MAP["Number of Classes"] =14;
+EQ_METRIC_MAP["Afferent Coupling"] =15;
+EQ_METRIC_MAP["Weighted Method Count"] =16;
+EQ_METRIC_MAP["Degree"] =17;
+EQ_METRIC_MAP["Normalized Distance"] =18;
+EQ_METRIC_MAP["Abstractness"] =19;
+EQ_METRIC_MAP["Instability"] =20;
+EQ_METRIC_MAP["OutDegree"] =21;
+EQ_METRIC_MAP["InDegree"] =22;
+EQ_METRIC_MAP["Coupling Between Object Classes"] =23;
+EQ_METRIC_MAP["Access to Foreign Data"] =24;
+EQ_METRIC_MAP["Number of Fields"] =25;
+EQ_METRIC_MAP["Specialization Index"] =26;
+EQ_METRIC_MAP["Class-Methods Lines of Code"] =27;
+EQ_METRIC_MAP["Number of Children"] =28;
+EQ_METRIC_MAP["Depth of Inheritance Tree"] =29;
+EQ_METRIC_MAP["Number of Methods"] =30;
+EQ_METRIC_MAP["Response For a Class"] =31;
+EQ_METRIC_MAP["Lack of Tight Class Cohesion"] =32;
+EQ_METRIC_MAP["Lack of Cohesion of Methods"] =33;
+EQ_METRIC_MAP["Number of Static Fields"] =34;
+EQ_METRIC_MAP["Lack of Cohesion Among Methods(1-CAM)"] =35;
+EQ_METRIC_MAP["CBO App"] =36;
+EQ_METRIC_MAP["Simple Response For a Class"] =37;
+EQ_METRIC_MAP["Number of Static Methods"] =38;
+EQ_METRIC_MAP["CBO Lib"] =39;
+EQ_METRIC_MAP["Number of Overridden Methods"] =40;
+var EQ_SELECTED_CLASS_METRIC 		= "Coupling";
+var EQ_SELECTED_PACKAGE_METRIC 	= "Coupling";
+var EQ_SELECTED_PROJECT_METRIC 	= "Class Lines of Code";
+var EQ_CLASS_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_CLASS_METRIC];
+var EQ_PACKAGE_METRIC_INDEX	= EQ_METRIC_MAP[EQ_SELECTED_PACKAGE_METRIC];
+var EQ_PROJECT_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_PROJECT_METRIC];
+var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
+var EQ_CLASS_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Class Lines of Code","Weighted Method Count","Degree","OutDegree","InDegree","Coupling Between Object Classes","Access to Foreign Data","Number of Fields","Specialization Index","Class-Methods Lines of Code","Number of Children","Depth of Inheritance Tree","Number of Methods","Response For a Class","Lack of Tight Class Cohesion","Lack of Cohesion of Methods","Number of Static Fields","Lack of Cohesion Among Methods(1-CAM)","CBO App","Simple Response For a Class","Number of Static Methods","CBO Lib","Number of Overridden Methods"];
+var EQ_PACKAGE_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Number of Entities","Class Lines of Code","Efferent Coupling","Number of Interfaces","Number of Classes","Afferent Coupling","Weighted Method Count","Degree","Normalized Distance","Abstractness","Instability","OutDegree","InDegree"];
+var EQ_PROJECT_METRICS = ["Number of Highly Problematic Classes","Number of Entities","Number of Problematic Classes","Class Lines of Code","Number of External Packages","Number of Packages","Number of External Entities"];
+function EQ_GET_COLOR(d) {
+if(d.metrics)
+return EQ_COLOR_OF_LEVELS[d.metrics[EQ_CLASS_METRIC_INDEX]];
+if(d.pmetrics)
+return EQ_COLOR_OF_LEVELS[d.pmetrics[EQ_PACKAGE_METRIC_INDEX]];
+if(d.prmetrics)
+return EQ_COLOR_OF_LEVELS[d.prmetrics[EQ_PROJECT_METRIC_INDEX]];
+return EQ_COLOR_OF_LEVELS[0];
+}
---
+++ project/codemr/SPRINT2Submission/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,0,0],[1,0,0,0,0],[0,1,0,0,0],[2,0,0,0,2],[2,2,0,0,0]]
+var packages = [{
+"name": " com.project.model", "color": " #3182bd"
+}
+,{
+"name": " com.project.repository", "color": " #6baed6"
+}
+,{
+"name": " com.project", "color": " #9ecae1"
+}
+,{
+"name": " com.project.controller", "color": " #c6dbef"
+}
+,{
+"name": " com.project.service", "color": " #e6550d"
+}
+];
