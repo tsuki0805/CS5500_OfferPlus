d699bf8d715b2098c1d741a8b1a1d4c126c1c01d
yzjin117 <yzjin117@uw.edu>
2023-04-03 01:26:42 -0700

    add front end using react.js and bootstrap

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DailyActivityDetail from "./DailyActivityDetailApi";
+
+function App() {
+  return (
+      <div>
+        <h1>5500 Fit!</h1>
+        <DailyActivityDetail />
+      </div>
+  );
+}
+
+export default App;
