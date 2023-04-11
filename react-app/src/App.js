9e70ff4a95b0adefa972f0dcd96fb5f4df854018
yzjin117 <yzjin117@uw.edu>
2023-04-10 18:15:52 -0700

    add new front end page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,38 @@
-import './App.css';
-import DailyActivityDetail from "./DailyActivityDetailApi";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import DailyActivityDetail from './DailyActivityDetailApi';
+import DailyActivityDetailComparison from './DailyActivityDetailComparison';
 
 function App() {
-  return (
-      <div>
-        <h1>5500 Fit!</h1>
-        <DailyActivityDetail />
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand">5500 Fit!</span>
+                    <div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/details">
+                                    Daily Activity Detail
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/comparison">
+                                    Daily Activity Distance Comparison & CO2 Emission
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/details" element={<DailyActivityDetail />} />
+                        <Route exact path="/comparison" element={<DailyActivityDetailComparison />} />
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
