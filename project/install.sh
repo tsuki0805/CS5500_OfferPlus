3bf97851852736894f4fc2f1b6757bfe3c5264dc
Liang Huang <yolanda0805@outlook.com>
2023-04-18 22:47:43 -0700

    added install.sh

--- project/install.sh
+++ project/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install Java
-sudo apt-get update
-sudo apt-get install -y openjdk-11-jdk
+apt-get update
+apt-get install -y openjdk-11-jdk
 
 # Install Maven
-sudo apt-get install -y maven
+apt-get install -y maven
 
 # Verify installation
 java -version
